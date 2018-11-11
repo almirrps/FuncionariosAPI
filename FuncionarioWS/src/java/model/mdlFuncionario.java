6dbcea6ec038ae23510525f6b6d71e515a2e3e3f
Almir <Almir@NOTE-ALMIR>
2018-11-10 22:57:12 -0200

    Criação de projeto WebService (API REstful) para testes com pacotes DAO e Model.

---
+++ FuncionarioWS/src/java/model/mdlFuncionario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Almir
+ */
+public class mdlFuncionario {
+    private String datacad;
+    private String cargo;
+    private String cpf;
+    private String nome;
+    private String ufnasc;
+    private Float salario;
+    private String status;
+
+    
+}
